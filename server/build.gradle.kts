27330c04eeb24e5ce00c5fd259eb590fe0d38f90
testinfected <vtence@gmail.com>
2022-02-25 15:45:29 -0500

    Add GraalVM configuration to the build
    To run the native image, run `gradle nativeRun`. To build the native image, use
    `gradle nativeCompile`.
    
    The `reflect-config.json` and `resource-config.json` files have been generated
    by the GraalVM agent using `gradle -Pagent nativeCompile` and browsing the application.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     id("kickstart")
     application
     id("com.google.cloud.tools.jib") version "3.2.0"
-}
+`    id("org.graalvm.buildtools.native") version "0.9.10"
+`}
 
 application {
     mainClass.set("kickstart.CLI")
@@ -24,6 +25,24 @@ dependencies {
     testRuntimeOnly(libs.hamcrest.library)
     testRuntimeOnly(libs.juniversalchardet)
     testRuntimeOnly(libs.simple)
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(17))
+            })
+
+            // Main options
+            imageName.set("kickstart") // The name of the native image, defaults to the project name
+            //debug.set(true) // Determines if debug info should be generated, defaults to false
+            //verbose.set(true) // Add verbose output, defaults to false
+            configurationFileDirectories.from(file("src/native"))
+            // Advanced options
+            buildArgs.add("--allow-incomplete-classpath")
+        }
+    }
 }
 
 jib {
