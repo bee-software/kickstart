8699d0297b3b34924aa0f65338cf4df4222b6124
testinfected <vtence@gmail.com>
2022-10-28 22:21:07 -0400

    Port PasswordHashTest to minutest

--- domain/src/test/kotlin/security/PasswordHashTest.kt
+++ domain/src/test/kotlin/security/PasswordHashTest.kt
@@ -4,35 +4,38 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.hasElement
 import com.natpryce.hamkrest.hasSize
-import kotlin.test.Test
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import dev.minutest.test
 
-class PasswordHashTest {
-    @Test
-    fun `creates hash from clear text password`() {
-        val hash = PasswordHash.create("clear password")
-        assertThat("hash value", hash.value, !equalTo("clear password"))
-        assertThat("valid password", hash.validate("clear password"), equalTo(true))
-        assertThat("invalid password", hash.validate("wrong password"), equalTo(false))
-
-        val same = PasswordHash.from(hash.value)
-        assertThat("same hash", same.validate("clear password"), equalTo(true))
-    }
-
-    @Test
-    fun `generates a 24 bytes salt`() {
-        val hash = PasswordHash.create("password")
-        val salt = salt(hash)
-        assertThat("salt size", salt.length, equalTo(24 * 2))
-    }
-
-    @Test
-    fun `generates unique random salts`() {
-        val salts = arrayListOf<String>()
-        repeat(100) {
+class PasswordHashTest : JUnit5Minutests {
+    fun salting() = rootContext {
+        test("generates a 24 bytes salt") {
             val hash = PasswordHash.create("password")
             val salt = salt(hash)
-            assertThat("salt collision", salts, !hasElement(salt))
-            salts.add(salt)
+            assertThat("salt size", salt.length, equalTo(24 * 2))
+        }
+
+        test("generates unique random salts") {
+            val salts = arrayListOf<String>()
+            repeat(100) {
+                val hash = PasswordHash.create("password")
+                val salt = salt(hash)
+                assertThat("salt collision", salts, !hasElement(salt))
+                salts.add(salt)
+            }
+        }
+
+        fun hashing() = rootContext {
+            test("creates hash from clear text password") {
+                val hash = PasswordHash.create("clear password")
+                assertThat("hash value", hash.value, !equalTo("clear password"))
+                assertThat("valid password", hash.validate("clear password"), equalTo(true))
+                assertThat("invalid password", hash.validate("wrong password"), equalTo(false))
+
+                val same = PasswordHash.from(hash.value)
+                assertThat("same hash", same.validate("clear password"), equalTo(true))
+            }
         }
     }
 
