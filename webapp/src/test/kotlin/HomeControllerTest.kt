b62f59f03bd3604a2ceadb12e8c721708f39a0b9
testinfected <vtence@gmail.com>
2022-10-29 12:21:54 -0400

    Convert HomeControllerTest to minutest

--- webapp/src/test/kotlin/HomeControllerTest.kt
+++ webapp/src/test/kotlin/HomeControllerTest.kt
@@ -3,25 +3,40 @@ package kickstart
 import com.vtence.molecule.Request
 import com.vtence.molecule.http.HttpStatus
 import com.vtence.molecule.testing.ResponseAssert.assertThat
+import dev.minutest.*
+import dev.minutest.junit.JUnit5Minutests
 import kickstart.security.Username
 import kickstart.security.bindFreshSession
 import kickstart.security.username
-import kotlin.test.Test
 
-class HomeControllerTest {
-    val view = TestView<Home>()
-    val home = HomeController(view)
 
-    @Test
-    fun `renders home page, with currently signed in user`() {
+class HomeControllerTest : JUnit5Minutests {
+    class Fixture {
+        val view = TestView<Home>()
+        val home = HomeController(view)
+
         val request = Request.get("/")
-        bindFreshSession(request).also { it.username = Username("alice@gmail.com") }
+        val session = bindFreshSession(request)
+    }
 
-        val response = home.render(request)
+    @Tests
+    fun tests() = rootContext<Fixture> {
+        given { Fixture() }
 
-        assertThat(response)
-            .hasStatus(HttpStatus.OK)
-            .isDone and
-                view renderedWith Home(Username("alice@gmail.com"))
+        context("when user signed in") {
+            beforeEach {
+                session.username = Username("alice@gmail.com")
+            }
+
+            test("renders page for currently signed in user") {
+                val response = home.render(request)
+
+                assertThat(response)
+                    .hasStatus(HttpStatus.OK)
+                    .isDone and
+                        view renderedWith Home(Username("alice@gmail.com"))
+            }
+        }
     }
 }
+
