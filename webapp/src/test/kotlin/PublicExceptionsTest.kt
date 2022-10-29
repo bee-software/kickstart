41ed427ca38591221332b35a4615da87bdfccfde
testinfected <vtence@gmail.com>
2022-10-29 12:40:25 -0400

    Convert PublicExceptionsTest to minutest

--- webapp/src/test/kotlin/PublicExceptionsTest.kt
+++ webapp/src/test/kotlin/PublicExceptionsTest.kt
@@ -1,57 +1,76 @@
 package kickstart
 
+import com.vtence.molecule.Application
 import com.vtence.molecule.Request
 import com.vtence.molecule.Response
 import com.vtence.molecule.http.HttpStatus
 import com.vtence.molecule.testing.ResourceLocator
 import com.vtence.molecule.testing.ResponseAssert.assertThat
+import dev.minutest.*
+import dev.minutest.junit.JUnit5Minutests
 import kickstart.i18n.locale
 import org.hamcrest.Matchers.containsString
 import java.nio.charset.StandardCharsets
 import java.util.*
-import kotlin.test.Test
 
 
-class PublicExceptionsTest {
+private class ExceptionPageFixture {
     val exceptions = PublicExceptions(ResourceLocator.locateOnClasspath("test/pages").toPath())
 
     val request: Request = Request.get("/")
 
-    @Test
-    fun `renders html exception page, named after status code`() {
-        val response = exceptions.then { Response.of(HttpStatus.BAD_GATEWAY).done() }
-            .handle(request)
-
-        assertThat(response)
-            .hasStatus(HttpStatus.BAD_GATEWAY)
-            .hasContentType("text/html; charset=utf-8")
-            .hasHeader("Content-Length", response.body().size(StandardCharsets.UTF_8).toString())
-            .hasBodyText(containsString("Bad Gateway (502)"))
+    fun handle(request: Request, next: Application): Response {
+        return exceptions.then(next).handle(request)
     }
+}
 
-    @Test
-    fun `renders localized version of page if possible`() {
-        request.locale = Locale.CANADA_FRENCH
-        val response = exceptions.then { Response.of(HttpStatus.BAD_GATEWAY).done() }
-            .handle(request)
+class PublicExceptionsTest : JUnit5Minutests {
+    @Tests
+    fun tests() = rootContext<ExceptionPageFixture> {
+        given { ExceptionPageFixture() }
 
-        assertThat(response).hasBodyText(containsString("Passerelle en panne"))
-    }
+        test("renders html exception page, named after status code") {
+            val response = handle(request) { Response.of(HttpStatus.BAD_GATEWAY).done() }
 
-    @Test
-    fun `falls back to non localized page otherwise`() {
-        request.locale = Locale.JAPANESE
-        val response = exceptions.then { Response.of(HttpStatus.BAD_GATEWAY).done() }
-            .handle(request)
+            assertThat(response)
+                .hasStatus(HttpStatus.BAD_GATEWAY)
+                .hasContentType("text/html; charset=utf-8")
+                .hasHeader("Content-Length", response.body().size(StandardCharsets.UTF_8).toString())
+                .hasBodyText(containsString("Bad Gateway (502)"))
+        }
 
-        assertThat(response).hasBodyText(containsString("Bad Gateway"))
-    }
+        test("leaves response unchanged if no matching error page can be found") {
+            val response = handle(request) { Response.ok().done("All good") }
 
-    @Test
-    fun `leaves response unchanged if no matching error page can be found`() {
-        val response = exceptions.then { Response.ok().done("All good") }
-            .handle(request)
+            assertThat(response).hasStatus(HttpStatus.OK).hasBodyText("All good")
+        }
 
-        assertThat(response).hasStatus(HttpStatus.OK).hasBodyText("All good")
+        context("when locale supported") {
+            beforeEach {
+                request.locale = Locale.CANADA_FRENCH
+            }
+
+            test("renders localized version of page") {
+                val response = handle(request) {
+                    Response.of(HttpStatus.BAD_GATEWAY).done()
+                }
+
+                assertThat(response).hasBodyText(containsString("Passerelle en panne"))
+            }
+        }
+
+        context("when locale not supported") {
+            beforeEach {
+                request.locale = Locale.JAPANESE
+            }
+
+            test("falls back to non localized page otherwise") {
+                val response = handle(request) {
+                    Response.of(HttpStatus.BAD_GATEWAY).done()
+                }
+
+                assertThat(response).hasBodyText(containsString("Bad Gateway"))
+            }
+        }
     }
 }
