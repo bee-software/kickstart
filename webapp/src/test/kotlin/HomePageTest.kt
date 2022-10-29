56221049b030e6409d76d974265b1e43f7cb89e6
testinfected <vtence@gmail.com>
2022-10-29 12:21:43 -0400

    Convert HomePageTest to minutest

--- webapp/src/test/kotlin/HomePageTest.kt
+++ webapp/src/test/kotlin/HomePageTest.kt
@@ -1,8 +1,8 @@
 package kickstart
 
-import com.natpryce.hamkrest.allElements
-import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.equalToIgnoringCase
+import dev.minutest.*
+import dev.minutest.junit.JUnit5Minutests
 import it.skrape.matchers.toBeNotPresent
 import it.skrape.selects.Doc
 import it.skrape.selects.html5.a
@@ -10,34 +10,40 @@ import it.skrape.selects.html5.main
 import it.skrape.selects.html5.menu
 import kickstart.security.Username
 import java.util.*
-import kotlin.test.Test
 
-class HomePageTest {
 
-    @Test
-    fun `is localized`() {
-        render(locale = Locale.FRENCH) {
-            eachAttribute("lang") should allElements(equalTo("fr"))
+class HomePageFixture(
+    private val content: Home = Home(),
+    private val locale: Locale = Locale.getDefault(),
+) {
+    fun render(relaxed: Boolean = true, expectations: Expectations): Doc {
+        return renderView("home", content, locale) {
+            this.relaxed = relaxed
+            expectations(this)
         }
     }
+}
 
-    @Test
-    fun `has title`() {
-        render {
-            titleText shouldBe "Kickstart"
+
+fun ContextBuilder<HomePageFixture>.showsTopMenu() {
+    context("shows top menu") {
+
+        test("has page title") {
+            render {
+                titleText shouldBe "Kickstart"
+            }
         }
-    }
 
-    @Test
-    fun `links back home`() {
-        render {
-            main {
-                menu {
-                    "li.active" {
-                        a {
-                            findFirst {
-                                text shouldBe "Home"
-                                attribute("href") shouldBe "/en"
+        test("links back home") {
+            render {
+                main {
+                    menu {
+                        "li.active" {
+                            a {
+                                findFirst {
+                                    text shouldBe "Home"
+                                    attribute("href") shouldBe "/en"
+                                }
                             }
                         }
                     }
@@ -45,77 +51,78 @@ class HomePageTest {
             }
         }
     }
+}
 
-    @Test
-    fun `links to login, when no user is signed in`() {
-        render() {
-            relaxed = true
 
-            main {
-                menu {
-                    findFirst("#login a") {
-                        text shouldBe "Log in"
-                        attribute("href") shouldBe "/en/login"
-                    }
+class HomePageTest : JUnit5Minutests {
+    @Tests
+    fun tests() = rootContext<HomePageFixture> {
+        context("when not signed in") {
+            given { HomePageFixture() }
 
-                    findAll(".avatar.image") { toBeNotPresent }
-                }
-            }
-        }
-    }
+            showsTopMenu()
 
-    @Test
-    fun `displays user dropdown menu when signed in, with avatar and signed in email`() {
-        render(Home(username = Username("alice@gmail.com"))) {
-            relaxed = true
+            test("links to login") {
+                render {
+                    main {
+                        menu {
+                            findFirst("#login a") {
+                                text shouldBe "Log in"
+                                attribute("href") shouldBe "/en/login"
+                            }
 
-            main {
-                menu {
-                    ".dropdown.item" {
-                        findFirst(".menu .header") {
-                            text should equalToIgnoringCase("alice@gmail.com")
-                        }
-
-                        findFirst(".avatar img") {
-                            attribute("src") shouldBe "//www.gravatar.com/avatar/0ce273d3249291c620af81403b14b3c1?d=mp&s=32"
+                            findAll(".avatar.image") { toBeNotPresent }
                         }
                     }
                 }
             }
         }
-    }
 
-    @Test
-    fun `offers logout option for signed in users`() {
-        render(Home(username = Username("alice@gmail.com"))) {
-            relaxed = true
+        context("when signed in") {
+            given { HomePageFixture(Home(username = Username("alice@gmail.com"))) }
 
-            main {
-                menu {
-                    findAll("#login a") { toBeNotPresent }
+            showsTopMenu()
 
-                    findFirst("form#logout") {
-                        attribute("action") shouldBe "/logout"
-                        attribute("method") shouldBe "post"
+            test("displays user avatar and signed in email in profile dropdown menu") {
+                render {
+                    main {
+                        menu {
+                            ".dropdown.item" {
+                                findFirst(".menu .header") {
+                                    text should equalToIgnoringCase("alice@gmail.com")
+                                }
 
-                        findFirst("input[name=_method]") {
-                            value should equalToIgnoringCase("delete")
+                                findFirst(".avatar img") {
+                                    attribute("src") shouldBe "//www.gravatar.com/avatar/0ce273d3249291c620af81403b14b3c1?d=mp&s=32"
+                                }
+                            }
                         }
-                        findFirst("button") {
-                            text shouldBe "Log out"
+                    }
+                }
+            }
+
+
+            test("offers logout option") {
+                render {
+                    main {
+                        menu {
+                            findAll("#login a") { toBeNotPresent }
+
+                            findFirst("form#logout") {
+                                attribute("action") shouldBe "/logout"
+                                attribute("method") shouldBe "post"
+
+                                findFirst("input[name=_method]") {
+                                    value should equalToIgnoringCase("delete")
+                                }
+                                findFirst("button") {
+                                    text shouldBe "Log out"
+                                }
+                            }
                         }
                     }
                 }
             }
         }
-    }
-
-
-    private fun render(
-        content: Home = Home(),
-        locale: Locale = Locale.getDefault(),
-        expectations: Expectations
-    ): Doc {
-        return renderView("home", content, locale, expectations)
     }
 }
