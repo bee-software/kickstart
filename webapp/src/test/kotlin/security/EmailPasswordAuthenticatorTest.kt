8a6792eed9fc4e2d3237d0eefefd33280a763ef7
testinfected <vtence@gmail.com>
2022-10-30 00:24:52 -0400

    Convert EmailPasswordAuthenticatorTest to minutest

--- webapp/src/test/kotlin/security/EmailPasswordAuthenticatorTest.kt
+++ webapp/src/test/kotlin/security/EmailPasswordAuthenticatorTest.kt
@@ -3,43 +3,56 @@ package kickstart.security
 import com.natpryce.hamkrest.absent
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.present
-import org.junit.jupiter.api.Test
+import dev.minutest.*
+import dev.minutest.junit.JUnit5Minutests
 import security.UserThat
 
-class EmailPasswordAuthenticatorTest {
-    val users = InMemoryUsers()
-    val authenticator = EmailPasswordAuthenticator(users)
 
-    @Test
-    fun `rejects credentials when no username provided`() {
-        assertThat(authenticator.authenticate(), absent())
+class EmailPasswordAuthenticatorTest : JUnit5Minutests {
+    class Fixture(
+        private val users: InMemoryUsers = InMemoryUsers(),
+        val authenticator: Authenticator = EmailPasswordAuthenticator(users),
+    ) : Authenticator by authenticator {
+
+        fun given(vararg users: User) {
+            users.forEach { this.users.add(it) }
+        }
     }
 
-    @Test
-    fun `rejects credential when no password provided`() {
-        assertThat(authenticator.authenticate("username"), absent())
-    }
+    @Tests
+    fun tests() = rootContext<Fixture> {
+        given { Fixture() }
 
-    @Test
-    fun `rejects credentials when no users have matching credentials`() {
-        val authenticated = authenticator.authenticate("unknown", "password")
-        assertThat("authenticated", authenticated, absent())
-    }
+        test("rejects credentials when no username provided") {
+            assertThat(authenticate(), absent())
+        }
 
-    @Test
-    fun `accepts credentials if passwords match`() {
-        users.add(User(Username("other"), PasswordHash.create("secret")))
-        users.add(User(Username("admin"), PasswordHash.create("secret")))
+        test("rejects credential when no password provided") {
+            assertThat(authenticate("username"), absent())
+        }
 
-        val authenticated = authenticator.authenticate("admin", "secret")
-        assertThat("authenticated", authenticated, present(UserThat.hasUsername("admin")))
-    }
+        context("with existing users") {
+            beforeEach {
+                given(
+                    User(Username("other"), PasswordHash.create("secret")),
+                    User(Username("admin"), PasswordHash.create("secret"))
+                )
+            }
 
-    @Test
-    fun `rejects credentials if passwords do not match`() {
-        users.add(User(Username("admin"), PasswordHash.create("secret")))
+            test("rejects credentials when no users have matching credentials") {
+                val authenticated = authenticate("unknown", "password")
+                assertThat("authenticated", authenticated, absent())
+            }
 
-        val authenticated = authenticator.authenticate("admin", "wrong secret")
-        assertThat("authenticated", authenticated, absent())
+            test("accepts credentials if passwords match") {
+                val authenticated = authenticate("admin", "secret")
+                assertThat("authenticated", authenticated, present(UserThat.hasUsername("admin")))
+            }
+
+            test("rejects credentials if passwords do not match") {
+                val authenticated = authenticate("admin", "wrong secret")
+                assertThat("authenticated", authenticated, absent())
+            }
+        }
     }
 }
