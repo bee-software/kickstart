48226ae56996e3938744764db3bcfe4034fb3521
testinfected <vtence@gmail.com>
2022-10-29 23:12:23 -0400

    Convert StatusEndpointTest to minutest

--- webapp/src/test/kotlin/telemetry/StatusEndpointTest.kt
+++ webapp/src/test/kotlin/telemetry/StatusEndpointTest.kt
@@ -3,19 +3,26 @@ package kickstart.telemetry
 import com.vtence.molecule.Request
 import com.vtence.molecule.http.HttpStatus
 import com.vtence.molecule.testing.ResponseAssert.assertThat
+import dev.minutest.Tests
+import dev.minutest.given
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import dev.minutest.test
 import kotlin.test.Test
 
-class StatusEndpointTest {
-    val endpoint = StatusEndpoint()
+class StatusEndpointTest : JUnit5Minutests {
+    @Tests
+    fun tests() = rootContext<StatusEndpoint> {
+        given { StatusEndpoint() }
 
-    @Test
-    fun `responds with ok in plain text`() {
-        val response = endpoint.get(Request.get("/status"))
+        test("responds with ok in plain text") {
+            val response = get(Request.get("/status"))
 
-        assertThat(response)
-            .isDone
-            .hasStatus(HttpStatus.OK)
-            .hasContentType("text/plain")
-            .hasBodyText("All green.")
+            assertThat(response)
+                .isDone
+                .hasStatus(HttpStatus.OK)
+                .hasContentType("text/plain")
+                .hasBodyText("All green.")
+        }
     }
 }
