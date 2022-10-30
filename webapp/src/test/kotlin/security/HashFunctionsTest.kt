c00f95817b95d76e8df132832ae1c6984aaf32fa
testinfected <vtence@gmail.com>
2022-10-30 00:25:00 -0400

    Convert HashFunctionsTest to minutest

--- webapp/src/test/kotlin/security/HashFunctionsTest.kt
+++ webapp/src/test/kotlin/security/HashFunctionsTest.kt
@@ -2,21 +2,26 @@ package kickstart.security
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.junit.jupiter.api.Test
-import javax.swing.text.html.parser.DTDConstants.MD
+import dev.minutest.Tests
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import dev.minutest.test
 
-class HashFunctionsTest {
-    @Test
-    fun `computes md5 checksum of content`() {
-        assertThat(
-            "md5", MD5("lorem ipsum").toHex(), equalTo("80a751fde577028640c419000e33eba6")
-        )
-    }
+class HashFunctionsTest : JUnit5Minutests {
+    @Tests
+    fun tests() = rootContext {
+        context("MD5") {
+            test("computes checksum of content") {
+                assertThat(
+                    "md5", MD5("lorem ipsum").toHex(), equalTo("80a751fde577028640c419000e33eba6")
+                )
+            }
 
-    @Test
-    fun `pads checksum with zeros`() {
-        assertThat(
-            "md5", MD5("363").toHex(), equalTo("00411460f7c92d2124a67ea0f4cb5f85")
-        )
+            test("pads checksum with zeros") {
+                assertThat(
+                    "md5", MD5("363").toHex(), equalTo("00411460f7c92d2124a67ea0f4cb5f85")
+                )
+            }
+        }
     }
 }
