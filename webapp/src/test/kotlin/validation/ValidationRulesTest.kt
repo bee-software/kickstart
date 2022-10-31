3d86be754348acc95bd24c3076f52cb01b765bf3
testinfected <vtence@gmail.com>
2022-10-30 22:04:48 -0400

    Convert ValidationRulesTest to minutest

--- webapp/src/test/kotlin/validation/ValidationRulesTest.kt
+++ webapp/src/test/kotlin/validation/ValidationRulesTest.kt
@@ -1,30 +1,39 @@
 package kickstart.validation
 
 import com.natpryce.hamkrest.assertion.assertThat
+import dev.minutest.Tests
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import dev.minutest.test
 import kickstart.isFailure
-import kotlin.test.Test
 
-class FailureTest {
-    @Test
-    fun `always fails, reporting offending value`() {
-        val validate = failure(Key("invalid"))
-        val result = validate("value")
-        assertThat(result, isFailure<String>(Violation(Key("invalid"), listOf("value"))))
-    }
-}
 
-class NotBlankTest {
-    @Test
-    fun `fails if value is null, reporting no value`() {
-        val validate = notBlank(Key("null"))
-        val result = validate(null)
-        assertThat(result, isFailure<String>(Violation(Key("null"))))
+class ValidationRulesTest : JUnit5Minutests {
+    @Tests
+    fun `failure tests`() = rootContext {
+        test("always fails, reporting offending value") {
+            val validate = failure(Key("invalid"))
+            val result = validate("value")
+            assertThat(result, isFailure<String>(Violation(Key("invalid"), listOf("value"))))
+        }
     }
 
-    @Test
-    fun `fails if value is blank, still reporting no value`() {
-        val validate = notBlank(Key("blank"))
-        val result = validate("   ")
-        assertThat(result, isFailure<String>(Violation(Key("blank"))))
+    @Tests
+    fun `not blank tests`() = rootContext {
+        context("value is null") {
+            test("fails and reports no value") {
+                val validate = notBlank(Key("null"))
+                val result = validate(null)
+                assertThat(result, isFailure<String>(Violation(Key("null"))))
+            }
+        }
+
+        context("value is blank") {
+            test("fails if value is blank, still reporting no value") {
+                val validate = notBlank(Key("blank"))
+                val result = validate("   ")
+                assertThat(result, isFailure<String>(Violation(Key("blank"))))
+            }
+        }
     }
 }
