9e6806746b1c41a25a104a60d840e4478dfb862d
testinfected <vtence@gmail.com>
2022-10-30 21:57:13 -0400

    Convert ValidationKeysTest to minutest

--- webapp/src/test/kotlin/validation/ValidationKeysTest.kt
+++ webapp/src/test/kotlin/validation/ValidationKeysTest.kt
@@ -2,47 +2,55 @@ package kickstart.validation
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import kotlin.test.Test
+import dev.minutest.Tests
+import dev.minutest.given
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import dev.minutest.test
+import kickstart.validation.ValidationFixture.*
 
 val foo by notBlank
 
-object Errors : ValidationKeys() {
-    val foo by notBlank
+
+class ValidationFixture {
     val bar by failure
-}
-
-
-class ValidationKeysTest {
-    val bar by failure
-
-    @Test
-    fun `keys defined as variables`() {
-        assertThat(foo.key, equalTo(Key("foo")))
-        assertThat(bar.key, equalTo(Key("bar")))
-    }
-
-    @Test
-    fun `keys in singleton objects`() {
-        assertThat(Errors.foo.key, equalTo(Key("errors.foo")))
-        assertThat(Errors.bar.key, equalTo(Key("errors.bar")))
-    }
-
-
     val foo_bar by failure
 
-    @Test
-    fun `underscores are replaced with hyphens`() {
-        assertThat(foo_bar.key, equalTo(Key("foo-bar")))
+    object Errors : ValidationKeys() {
+        val foo by notBlank
+        val bar by failure
     }
+
 
     object outer : ValidationKeys() {
         object inner : ValidationKeys() {
             val baz by failure
         }
     }
+}
 
-    @Test
-    fun `definitions in deep nested keys`() {
-        assertThat(outer.inner.baz.key, equalTo(Key("outer.inner.baz")))
+
+class ValidationKeysTest : JUnit5Minutests {
+    @Tests
+    fun tests() = rootContext<ValidationFixture> {
+        given { ValidationFixture() }
+
+        test("keys defined as variables") {
+            assertThat(foo.key, equalTo(Key("foo")))
+            assertThat(bar.key, equalTo(Key("bar")))
+        }
+
+        test("keys in singleton objects") {
+            assertThat(Errors.foo.key, equalTo(Key("errors.foo")))
+            assertThat(Errors.bar.key, equalTo(Key("errors.bar")))
+        }
+
+        test("underscores are replaced with hyphens") {
+            assertThat(foo_bar.key, equalTo(Key("foo-bar")))
+        }
+
+        test("definitions in deep nested keys") {
+            assertThat(outer.inner.baz.key, equalTo(Key("outer.inner.baz")))
+        }
     }
 }
